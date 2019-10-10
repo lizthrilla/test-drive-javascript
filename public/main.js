9e34e3b608958d621f2669f11cd294cc1cebcde1
Liz Tiller <elizabeth.tiller@gmail.com>
2019-10-09 20:26:56 -0400

    part 2

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+// shorthand for document.querySelector
+const qs = (element) => document.querySelector(element)
+
 // Setting starting scores in javascript memory to 0
 let teamOneScore = 0
 let teamTwoScore = 0
@@ -55,8 +58,40 @@ function teamTwoScoreReachesTen () {
   teamTwoScoreText.style.color = 'blue'
 }
 
+// when called the name for team one will update
+const updateTeamOneName = () => {
+  const teamOneName = document.querySelector('input.team-1-input').value
+  console.log(teamOneName)
+  document.querySelector('.team-1-name').textContent = teamOneName
+  document.querySelector('input.team-1-input').value = 'Enter name'
+}
+
+const updateTeamTwoName = () => {
+  const teamTwoName = document.querySelector('input.team-2-input').value
+  console.log(teamTwoName)
+  document.querySelector('.team-2-name').textContent = teamTwoName
+  document.querySelector('input.team-2-input').value = 'Enter name'
+}
+
+const emptyValueOne = () => {
+  document.querySelector('input.team-1-input').value = ''
+}
+
+const emptyValueTwo = () => {
+  document.querySelector('input.team-2-input').value = ''
+}
+
+const enterPrompt = () => {
+  qs('.team-1-input').value = 'Enter name'
+}
+
 // creating main function that is called when script tag loads
 const main = () => {
+  qs('input.team-1-input').value = 'Enter name'
+  qs('input.team-1-input').addEventListener('focus', emptyValueOne)
+  qs('.team-1-input').addEventListener('blur', enterPrompt)
+  // qs('input.team-2-input').value = 'Enter name'
+  qs('.team-2-input').addEventListener('focus', emptyValueTwo)
   // telling team 1 add 1 button to listen for a click and run function: teamOneAddOne
   document.querySelector('button.team-1-add-1-button').addEventListener('click', teamOneAddOne)
 
@@ -68,6 +103,10 @@ const main = () => {
 
   // telling team 2 subtract 1 button to listen for click and run function: teamTWoSubtractOne
   document.querySelector('.team-2-subtract-1-button').addEventListener('click', teamTwoSubtractOne)
+
+  document.querySelector('button.update-team-1-name').addEventListener('click', updateTeamOneName)
+
+  document.querySelector('button.update-team-2-name').addEventListener('click', updateTeamTwoName)
 }
 
 // telling the whole document to listen for the DOM's content to load and then run main function
